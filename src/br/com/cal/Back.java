cd57c6727395d8696668317e0a0c9253591d5bb0
vzanbr <128941678+vzanbr@users.noreply.github.com>
2024-01-17 14:58:07 -0300

    Calculadora com classe
    Desafio Fourcamp

---
+++ src/br/com/cal/Back.java
@@ -0,0 +1,38 @@
+package br.com.cal;
+
+public class Back {
+
+    public double calcular(double numero1, double numero2, char operacao) {
+        switch (operacao) {
+            case '+':
+                return Soma (numero1, numero2);
+            case '-':
+                return Subtracao (numero1, numero2);
+            case '*':
+                return Multiplicacao (numero1, numero2);
+            case '/':
+                return Divisao (numero1, numero2);
+            default:
+                throw new IllegalArgumentException("Falha no calculo");
+        }
+
+        }
+        private double Soma(double numero1, double numero2) {
+            return numero1 + numero2;
+        }
+
+        private double Subtracao(double numero1, double numero2) {
+            return numero1 - numero2;
+        }
+
+        private double Multiplicacao(double numero1, double numero2) {
+            return numero1 * numero2;
+        }
+
+        private double Divisao(double numero1, double numero2) {
+            if (numero2 == 0) {
+                throw new ArithmeticException("Valor invalido!");
+        }
+            return numero1 / numero2;
+    }
+}
---
+++ src/br/com/cal/Main.java
@@ -0,0 +1,26 @@
+package br.com.cal;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner memoria = new Scanner(System.in);
+
+        Back cal = new Back();
+
+        System.out.println(" numero 1 = ");
+        double numero1 = memoria.nextDouble();
+
+        System.out.println(" numero 2 = ");
+        double numero2 = memoria.nextDouble();
+
+        System.out.println(" Qual operador ? + , - , * , / ");
+        char operacao = memoria.next().charAt(0);
+
+        double resultado = cal.calcular(numero1, numero2, operacao);
+        System.out.println("Resultado : " + resultado);
+
+        memoria.close();
+    }
+}
