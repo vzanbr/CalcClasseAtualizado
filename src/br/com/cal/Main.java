56167ec5831cc9bbf1949cba31e430ef4c9be63e
vzanbr <128941678+vzanbr@users.noreply.github.com>
2024-01-17 16:23:12 -0300

    Feature: Incluindo Ficha de cadastro.

--- src/br/com/cal/Main.java
+++ src/br/com/cal/Main.java
@@ -7,20 +7,39 @@ public class Main {
 
         Scanner memoria = new Scanner(System.in);
 
+        Usuario dados = new Usuario();
+
+        System.out.println("================================");
+        System.out.println("    Calculadora Educ360");
+        System.out.println("================================");
+
+        System.out.println("Faça seu Cadastro");
+        System.out.println("Qual é seu nome?");
+        dados.nome = memoria.nextLine();
+        System.out.println("Qual é sua idade?");
+        dados.idade = memoria.nextInt();
+
+        System.out.println("====================================");
+        System.out.println("Ola, " + dados.nome + " Seja bem vindo! ");
+        System.out.println("Você tem, " + dados.idade + " anos! ");
+        System.out.println("====================================");
+
+
         Calculadora cal = new Calculadora();
 
-        System.out.println(" numero 1 = ");
+        System.out.println(" Digite o primeiro valor = ");
         double numero1 = memoria.nextDouble();
 
-        System.out.println(" numero 2 = ");
+        System.out.println(" Digite o segundo valor = ");
         double numero2 = memoria.nextDouble();
 
-        System.out.println(" Qual operador ? + , - , * , / ");
+        System.out.println(" Selecione Operador. ");
         char operacao = memoria.next().charAt(0);
 
         double resultado = cal.calcular(numero1, numero2, operacao);
-        System.out.println("Resultado : " + resultado);
-
+        System.out.println("==============\n "+"Resultado : " + resultado + "\n===============");
+        System.out.println("Programa Finalizado!");
+        System.out.println("Programa by Gabriel Alves de Lima!");
         memoria.close();
     }
 }
